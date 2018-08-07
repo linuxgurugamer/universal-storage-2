e527296a1342639667e57c3b77b1b124108f8f21
DMagic <david.grandy@gmail.com>
2018-08-07 12:33:28 -0400

    1.3.1 source files

---
+++ 1.3.1/Source/UniversalStorage/USModuleStructuralNode.cs
@@ -0,0 +1,11 @@
+﻿
+namespace UniversalStorage
+{
+    public class USModuleStructuralNode : ModuleStructuralNode, IActivateOnDecouple
+    {          
+        new public void DecoupleAction(string s, bool b)
+        {
+
+        }
+    }
+}
