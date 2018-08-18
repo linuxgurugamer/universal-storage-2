0aba55c034e18bca26cd40084952230638a356a3
DMagic <david.grandy@gmail.com>
2018-08-18 14:27:50 -0400

    Code for the stock variant UI functions

---
+++ 1.4.5/Universal Storage Source 1.4.5/UniversalStorage/StockVariants/UI_USVariantSelector.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniversalStorage.StockVariants
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Field)]
+    public class UI_USVariantSelector : UI_Control
+    {
+        public List<USVariantInfo> Variants = new List<USVariantInfo>();
+    }
+}
