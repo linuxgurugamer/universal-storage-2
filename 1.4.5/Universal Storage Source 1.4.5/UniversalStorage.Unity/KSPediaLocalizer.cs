c128978f48021e9ee53c5b5abd1af5737c1209d5
DMagic <david.grandy@gmail.com>
2018-08-18 14:25:24 -0400

    Upload all of the KSPedia source code files

---
+++ 1.4.5/Universal Storage Source 1.4.5/UniversalStorage.Unity/KSPediaLocalizer.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+namespace UniversalStorage.Unity
+{
+    public class LocalizeKSPedia : UnityEvent<KSPediaLocalizer, string> { }
+
+    [RequireComponent(typeof(Text))]
+    public class KSPediaLocalizer : MonoBehaviour
+    {
+        public static LocalizeKSPedia onLocalize = new LocalizeKSPedia();
+
+        [SerializeField]
+        private string m_LocalizationTag = "";
+
+        private Text _text;
+        
+        private void Awake()
+        {
+            _text = GetComponent<Text>();
+        }
+
+        private void OnEnable()
+        {
+            onLocalize.Invoke(this, m_LocalizationTag);
+        }
+
+        public void UpdateText(string text)
+        {
+            if (_text != null)
+                _text.text = text;
+        }
+    }
+}
