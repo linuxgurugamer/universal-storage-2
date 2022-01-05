27a9e30f2c0e5ce278ba3c91a968e2d80d04bcbe
Jonathan Bayer <linuxgurugamer@gmail.com>
2022-01-04 23:35:49 -0500

    Added :LAST to the NerteaCryoMassRebalance.cfg  Changed :FINAL to LAST[UniversalStorage2] in the MassResourceRebalance.cfg  Fixed GuidanceComputer to only be available when kOs is installed  Added resize patch to reduce size of parts very slightly by about 2.25%, to better fix stock parts  Changed MassResourceRebalance.cfg to change FINAL to LAST[UniversalStorage2]  Following changes/additions to support IFILS, currently under development:   Fixed OxygenWedge for IFILS, to only hold liquid O2   Updated ComboLivesupportWedge to support IFILS   Updated FoodWedge with correct names for IFILS   Updated Elektron to not be available when IFILS is installed  Updated version file

--- deploy.bat
+++ deploy.bat
@@ -8,6 +8,9 @@ rem VERSIONFILE is the name of the version file, usually the same as GAMEDATA,
 rem    but not always
 
 set H=%KSPDIR%
+
+set H=R:\KSP_1.12.3_IFI_US2
+
 set GAMEDIR=UniversalStorage2
 set GAMEDATA="GameData"
 set VERSIONFILE=%GAMEDIR%.version
