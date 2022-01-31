773821684c7d525bb9900f9e9ca53581f5c03639
Jonathan Bayer <linuxgurugamer@gmail.com>
2022-01-30 22:45:10 -0500

    Fixed Microdawn not showing up when Restock is installed  Fixed microsats generating nullrefs when retracting antenna in editor

--- USSourceDev/UniversalStorage/AssemblyVersion.cs
+++ USSourceDev/UniversalStorage/AssemblyVersion.cs
@@ -10,6 +10,6 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("4.0.0.2")]
- [assembly: AssemblyFileVersion("4.0.0.2")]
- [assembly: AssemblyInformationalVersion("v4.0.0.2")]
+ [assembly: AssemblyVersion("4.0.0.3")]
+ [assembly: AssemblyFileVersion("4.0.0.3")]
+ [assembly: AssemblyInformationalVersion("v4.0.0.3")]
