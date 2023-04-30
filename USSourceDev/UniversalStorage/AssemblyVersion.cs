30ea5f210b8a2115efb8c2887e21ef5e6ea35e57
Jonathan Bayer <linuxgurugamer@gmail.com>
2023-04-30 17:21:58 -0400

    Updated verion in release data

--- USSourceDev/UniversalStorage/AssemblyVersion.cs
+++ USSourceDev/UniversalStorage/AssemblyVersion.cs
@@ -10,6 +10,6 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("4.0.1.0")]
- [assembly: AssemblyFileVersion("4.0.1.0")]
- [assembly: AssemblyInformationalVersion("v4.0.1.0")]
+ [assembly: AssemblyVersion("4.0.1.1")]
+ [assembly: AssemblyFileVersion("4.0.1.1")]
+ [assembly: AssemblyInformationalVersion("v4.0.1.1")]
